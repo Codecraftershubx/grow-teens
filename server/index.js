a8afc681e9a9bbb053548303eea52dd38ef5a2a2
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-03-02 06:37:23 +0100

    :star: feat: Set up Server Codebase and Project Structure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.SERVER_PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
