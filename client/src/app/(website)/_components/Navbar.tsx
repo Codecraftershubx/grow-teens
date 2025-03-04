fd4b2c060a3c8d642cf19fd717ff5e57034d2a4b
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-03-04 23:03:46 +0100

    :star: feat: Build Landing Page Hero Section

---
+++ client/src/app/(website)/_components/Navbar.tsx
@@ -0,0 +1,394 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { RxChevronDown, RxChevronRight } from "react-icons/rx";
+
+const useDropdownMenu = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev);
+  const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
+  const openDropdown = () => setIsDropdownOpen(true);
+  const closeDropdown = () => setIsDropdownOpen(false);
+
+  const animateMobileMenu = isMobileMenuOpen ? "open" : "close";
+  const animateMobileMenuButtonSpan = isMobileMenuOpen
+    ? ["open", "rotatePhase"]
+    : "closed";
+  const animateDropdownMenu = isDropdownOpen ? "open" : "close";
+  const animateDropdownMenuIcon = isDropdownOpen ? "rotated" : "initial";
+
+  return {
+    toggleMobileMenu,
+    toggleDropdown,
+    openDropdown,
+    closeDropdown,
+    animateMobileMenu,
+    animateMobileMenuButtonSpan,
+    animateDropdownMenu,
+    animateDropdownMenuIcon,
+  };
+};
+
+export function Navbar() {
+  const useActive = useDropdownMenu();
+
+  return (
+    <section
+      id="navbar"
+      className="relative z-[999] flex w-full items-center justify-between border-b border-border-primary bg-background-primary lg:min-h-18 lg:px-[5%]"
+    >
+      <div className="size-full lg:flex lg:items-center lg:justify-between">
+        <div className="lg:flex">
+          <div className="flex min-h-16 items-center justify-between px-[5%] md:min-h-18 lg:min-h-full lg:px-0">
+            <a href="#" color="text-text-primary" className="text-lg lg:text-xl">
+             GROW TEENS
+            </a>
+            <button
+              className="-mr-2 flex size-12 flex-col items-center justify-center lg:hidden"
+              onClick={useActive.toggleMobileMenu}
+            >
+              <motion.span
+                className="my-[3px] h-0.5 w-6 bg-black"
+                animate={useActive.animateMobileMenuButtonSpan}
+                variants={{
+                  open: { translateY: 8, transition: { delay: 0.1 } },
+                  rotatePhase: { rotate: -45, transition: { delay: 0.2 } },
+                  closed: {
+                    translateY: 0,
+                    rotate: 0,
+                    transition: { duration: 0.2 },
+                  },
+                }}
+              />
+              <motion.span
+                className="my-[3px] h-0.5 w-6 bg-black"
+                animate={useActive.animateMobileMenu}
+                variants={{
+                  open: { width: 0, transition: { duration: 0.1 } },
+                  closed: {
+                    width: "1.5rem",
+                    transition: { delay: 0.3, duration: 0.2 },
+                  },
+                }}
+              />
+              <motion.span
+                className="my-[3px] h-0.5 w-6 bg-black"
+                animate={useActive.animateMobileMenuButtonSpan}
+                variants={{
+                  open: { translateY: -8, transition: { delay: 0.1 } },
+                  rotatePhase: { rotate: 45, transition: { delay: 0.2 } },
+                  closed: {
+                    translateY: 0,
+                    rotate: 0,
+                    transition: { duration: 0.2 },
+                  },
+                }}
+              />
+            </button>
+          </div>
+          <motion.div
+            variants={{
+              open: { height: "var(--height-open, 100dvh)" },
+              close: { height: "var(--height-closed, 0)" },
+            }}
+            initial="close"
+            exit="close"
+            animate={useActive.animateMobileMenu}
+            transition={{ duration: 0.4 }}
+            className="overflow-auto px-[5%] lg:ml-6 lg:flex lg:items-center lg:px-0 lg:[--height-closed:auto] lg:[--height-open:auto]"
+          >
+            <a
+              href="#"
+              className="block py-3 text-md first:pt-7 lg:px-4 lg:py-6 lg:text-base first:lg:pt-6"
+            >
+              About Us
+            </a>
+            <a
+              href="#"
+              className="block py-3 text-md first:pt-7 lg:px-4 lg:py-6 lg:text-base first:lg:pt-6"
+            >
+              Our Programs
+            </a>
+            <a
+              href="#"
+              className="block py-3 text-md first:pt-7 lg:px-4 lg:py-6 lg:text-base first:lg:pt-6"
+            >
+              Get Involved
+            </a>
+            <div
+              onMouseEnter={useActive.openDropdown}
+              onMouseLeave={useActive.closeDropdown}
+            >
+              <button
+                className="flex w-full items-center justify-between gap-x-2 py-3 text-center text-md lg:w-auto lg:flex-none lg:justify-start lg:px-4 lg:py-6 lg:text-base cursor-pointer"
+                onClick={useActive.toggleDropdown}
+              >
+                <span>Resources</span>
+                <motion.span
+                  variants={{
+                    rotated: { rotate: 180 },
+                    initial: { rotate: 0 },
+                  }}
+                  animate={useActive.animateDropdownMenuIcon}
+                  transition={{ duration: 0.3 }}
+                >
+                  <RxChevronDown />
+                </motion.span>
+              </button>
+              <motion.div
+                variants={{
+                  open: {
+                    visibility: "visible",
+                    opacity: 1,
+                    height: "var(--height-open, auto)",
+                  },
+                  close: {
+                    visibility: "hidden",
+                    opacity: "0",
+                    height: "var(--height-close, 0)",
+                  },
+                }}
+                initial="close"
+                exit="close"
+                animate={useActive.animateDropdownMenu}
+                transition={{ duration: 0.3 }}
+                className="bottom-auto left-0 top-full w-full min-w-full max-w-full overflow-hidden bg-background-primary lg:absolute lg:w-[100vw] lg:border-b lg:border-border-primary lg:px-[5%] lg:[--height-close:auto]"
+              >
+                <div className="mx-auto flex size-full max-w-full items-center justify-between">
+                  <div className="w-full lg:flex">
+                    <div className="grid flex-1 gap-x-8 gap-y-6 py-4 pr-8 md:grid-cols-2 md:px-0 md:py-8 lg:py-8 lg:pr-8">
+                      <div className="grid auto-rows-max grid-cols-1 grid-rows-[max-content] gap-y-2 md:gap-y-4">
+                        <h4 className="text-sm font-semibold leading-[1.3]">
+                          Explore Our Work
+                        </h4>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 1"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Youth Empowerment</h5>
+                            <p className="hidden text-sm md:block">
+                              Empowering teens for a brighter future
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 2"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Digital Skills</h5>
+                            <p className="hidden text-sm md:block">
+                              Learn essential digital competencies for success
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 3"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Entrepreneurship</h5>
+                            <p className="hidden text-sm md:block">
+                              Start your own business with our guidance
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 4"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Leadership</h5>
+                            <p className="hidden text-sm md:block">
+                              Become a leader in your community
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="grid auto-rows-max grid-cols-1 grid-rows-[max-content] gap-y-2 md:gap-y-4">
+                        <h4 className="text-sm font-semibold leading-[1.3]">
+                          Latest Articles
+                        </h4>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 5"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Success Stories</h5>
+                            <p className="hidden text-sm md:block">
+                              Inspiring journeys of our empowered teens
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 6"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Upcoming Events</h5>
+                            <p className="hidden text-sm md:block">
+                              Join us for exciting workshops and seminars
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 7"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">
+                              Volunteer Opportunities
+                            </h5>
+                            <p className="hidden text-sm md:block">
+                              Make a difference in young lives today
+                            </p>
+                          </div>
+                        </a>
+                        <a
+                          href="#"
+                          className="grid w-full grid-cols-[max-content_1fr] items-start gap-x-3 py-2"
+                        >
+                          <div className="flex size-6 flex-col items-center justify-center">
+                            <img
+                              src="https://d22po4pjz3o32e.cloudfront.net/relume-icon.svg"
+                              alt="Icon 8"
+                            />
+                          </div>
+                          <div className="flex flex-col items-start justify-center">
+                            <h5 className="font-semibold">Community Impact</h5>
+                            <p className="hidden text-sm md:block">
+                              See how we change lives across Africa
+                            </p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                    <div className="max-w-none relative flex flex-1 p-6 md:py-8 md:pl-8 md:pr-0 lg:max-w-md">
+                      <div className="relative z-10 grid w-full auto-cols-fr auto-rows-max grid-cols-1 grid-rows-[max-content_max-content] gap-4">
+                        <h4 className="text-sm font-semibold leading-[1.3]">
+                          From Our Blog
+                        </h4>
+                        <div className="grid auto-cols-fr grid-cols-1 grid-rows-[auto_auto] items-start gap-y-2 lg:grid-rows-[auto]">
+                          <a
+                            href="#"
+                            className="flex auto-cols-fr grid-cols-[0.6fr_1fr] flex-col gap-x-6 py-2 md:grid"
+                          >
+                            <div className="relative w-full pt-[66.66%]">
+                              <img
+                                src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image-landscape.svg"
+                                alt="Relume placeholder image 1"
+                                className="absolute inset-0 size-full object-cover"
+                              />
+                            </div>
+                            <div className="rt-4 mt-4 flex flex-col justify-start md:mt-0">
+                              <h5 className="mb-1 font-semibold">
+                                Empowerment Insights
+                              </h5>
+                              <p className="text-sm">
+                                Discover tips for youth empowerment and growth
+                              </p>
+                              <div className="mt-1.5">
+                                <button className="text-sm underline">
+                                  Read more
+                                </button>
+                              </div>
+                            </div>
+                          </a>
+                          <a
+                            href="#"
+                            className="flex auto-cols-fr grid-cols-[0.6fr_1fr] flex-col gap-x-6 py-2 md:grid"
+                          >
+                            <div className="relative w-full pt-[66.66%]">
+                              <img
+                                src="https://d22po4pjz3o32e.cloudfront.net/placeholder-image-landscape.svg"
+                                alt="Relume placeholder image 2"
+                                className="absolute inset-0 size-full object-cover"
+                              />
+                            </div>
+                            <div className="rt-4 mt-4 flex flex-col justify-start md:mt-0">
+                              <h5 className="mb-1 font-semibold">
+                                Success Strategies
+                              </h5>
+                              <p className="text-sm">
+                                Learn how to thrive in today&apos;s world
+                              </p>
+                              <div className="mt-1.5">
+                                <button className="text-sm underline">
+                                  Read more
+                                </button>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="flex items-center">
+                          <button className="text-sm underline flex items-center gap-x-1">
+                            See all articles <RxChevronRight />
+                          </button>
+                        </div>
+                      </div>
+                      <div className="absolute bottom-0 left-0 right-auto top-0 min-w-full bg-background-secondary lg:min-w-[100vw]" />
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+            <div className="mt-6 flex w-full flex-col gap-y-4 pb-24 lg:hidden lg:pb-0">
+              <button className="w-full">Button</button>
+              <button className="w-full">Button</button>
+            </div>
+          </motion.div>
+        </div>
+        <div className="hidden lg:flex lg:gap-4">
+          <button>Join</button>
+          <button>Start</button>
+        </div>
+      </div>
+    </section>
+  );
+}
