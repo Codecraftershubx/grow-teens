ce7f19aaa4861b2f6bf853d74cdb6f71bf6cc3b3
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-03-08 08:01:46 +0100

    :star: feat: Add app icon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Roboto_Flex, Istok_Web } from "next/font/google";
 import Provider from "./provider";
 import "./globals.css";
+import GrowTeensLogo from "@public/icons/logo.svg";
+import GrowTeensLogoThumbnailImage from "@public/icons/logo.svg";
 
 const robotoFlex = Roboto_Flex({
   variable: "--font-roboto-flex",
@@ -14,9 +16,35 @@ const istokWeb = Istok_Web({
   subsets: ["latin"],
 });
 
+const appName = "GrowTeens";
+// const appUrl = "";
+const appMetaTitle = `${appName} - Empowering African Teens for a Brighter Future.`;
+const imageAlt = 'GrowTeens Logo';
+const appMetaDescription =
+  "Empowering African Teens for a Brighter Future. #GrowTeens #Health #Education #Empowerment #Leadership #Mentorship";
+
+
 export const metadata: Metadata = {
-  title: "Grow Teens",
-  description: "Empowering African Teens for a Brighter Future.",
+  title: appName,
+  description: appMetaTitle,
+  openGraph: {
+    type: "website",
+    title: appMetaTitle,
+    description: appMetaDescription,
+    images: [
+      {
+        url: GrowTeensLogoThumbnailImage.src,
+        alt: imageAlt,
+      },
+    ],
+  },
+  icons: [
+    {
+      url: GrowTeensLogo.src,
+      type: "image/png",
+      sizes: "32x32",
+    },
+  ],
 };
 
 export default function RootLayout({
